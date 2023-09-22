12db336e0aed5cea2f7dc69097f9ac1945174381
Thomas Zhao <tzhao@cognitivetalentsolution.com>
2023-09-22 11:29:46 -0700

    feat: boilerplate legal stuff

---
+++ apps/web/src/routes/privacy-policy/+page.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+</script>
+
+<div class="flex flex-col items-center justify-center min-h-screen bg-surface-800 font-mono">
+  <div
+    class="max-w-screen-md w-full p-4 md:p-8 rounded-lg shadow-lg text-slate-300"
+    style="width: 50%; max-width: 64rem;"
+  >
+    <h1 class="text-center text-4xl font-semibold mb-4">Cosaga Privacy Policy</h1>
+    <p class="text-center font-thin"><strong>Last Updated: 2023-09-22</strong></p>
+
+    <h2>Introduction</h2>
+    <p>
+      Welcome to Cosaga, a platform focused on collaborations, meaningful connections, and creative
+      sharing ("Service"). This Privacy Policy aims to help you understand how we process, store,
+      and protect your personal information when you use our Service. If you do not agree with our
+      policies and practices, please do not use our Service.
+    </p>
+
+    <h2>What Personal Information Do We Process?</h2>
+    <p>
+      When you visit or use our Services, we may collect and process personal data based on your
+      interactions and usage. This data will only be used to improve, provide, and secure the
+      Service.
+    </p>
+
+    <h2>Sensitive Personal Information</h2>
+    <p>
+      We do not process sensitive personal information unless explicitly provided by you for
+      specific services.
+    </p>
+
+    <h2>Information from Third Parties</h2>
+    <p>We do not receive personal information from third parties.</p>
+
+    <h2>How We Process Your Information</h2>
+    <p>
+      We process your data to provide, improve, and administer our Services. We also use your
+      information to communicate with you, for security and fraud prevention, and to comply with
+      relevant laws. We only process your data when we have a legal basis for doing so.
+    </p>
+
+    <h2>Sharing Personal Information</h2>
+    <p>
+      We may share your information under specific conditions and only with entities that ensure the
+      confidentiality and security of your data.
+    </p>
+
+    <h2>Data Security</h2>
+    <p>
+      We implement both organizational and technical measures to safeguard your personal
+      information. However, no method of transmission or storage is 100% secure.
+    </p>
+
+    <h2>Your Rights</h2>
+    <p>
+      Depending on your location, you may have specific rights under applicable privacy laws
+      concerning your personal data. Please refer to our detailed GDPR-compliant procedures for more
+      information.
+    </p>
+
+    <h2>How to Exercise Your Rights</h2>
+    <p>
+      To exercise your rights, you can contact us directly or fill out our data subject request
+      form.
+    </p>
+
+    <h2>Contact Information</h2>
+    <p>If you have any questions or concerns regarding this policy, please contact us at:</p>
+    <ul>
+      <li>Email: tommy@cosaga.app</li>
+      <li>Phone: (737) 471-9337</li>
+    </ul>
+  </div>
+</div>
+
+<style>
+  h2 {
+    font-size: 1.5rem;
+    margin-bottom: 0.75rem;
+    margin-top: 1.5rem;
+  }
+</style>
---
+++ apps/web/src/routes/tos/+page.svelte
@@ -0,0 +1,117 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+</script>
+
+<div class="flex flex-col items-center justify-center min-h-screen bg-surface-800 font-mono">
+  <div
+    class="max-w-screen-md w-full p-4 md:p-8 rounded-lg shadow-lg text-slate-300"
+    style="width: 50%; max-width: 64rem;"
+  >
+    <h1 class="text-center text-4xl font-semibold mb-4">Cosaga Terms of Service</h1>
+    <p class="text-center font-thin"><strong>Last Updated: 2023-09-21</strong></p>
+
+    <h2>1. Introduction</h2>
+    <p>
+      Welcome to Cosaga, a social platform designed around collaborations, meaningful connections,
+      and the sharing of creative work ("Service").
+    </p>
+    <p>
+      By accessing or using our Service, you agree to comply with and be bound by these Terms of
+      Service ("Terms") and the <a href="/privacy-policy">Privacy Policy</a>. Please read both
+      documents carefully, as they contain important information regarding your legal rights and
+      obligations.
+    </p>
+
+    <h2>2. Eligibility</h2>
+    <p>
+      You must be at least 16 years of age to use the Service. If you are under the age of 16, you
+      must obtain explicit consent from a parent or guardian to use this Service. By using the
+      Service, you represent and warrant that you meet these requirements.
+    </p>
+
+    <h2>3. Privacy Policy</h2>
+    <p>
+      Please refer to our Privacy Policy for information on how we collect, use, and disclose your
+      personal information.
+    </p>
+
+    <h2>4. Account Registration</h2>
+    <p>
+      You are required to register for an account to access certain features of the Service. You
+      agree to provide accurate, current, and complete information during the registration process.
+      We will collect only the data that is strictly necessary to provide you with the Service.
+    </p>
+
+    <h2>5. Usage Guidelines</h2>
+    <ul>
+      <li>You agree not to post content that is illegal, harmful, or offensive.</li>
+      <li>
+        You agree to abide by the limitations set forth by our features designed to limit the supply
+        of publicity on the platform.
+      </li>
+      <li>
+        You agree not to impersonate others or misrepresent your affiliation with any person or
+        entity.
+      </li>
+    </ul>
+
+    <h2>6. User Content</h2>
+    <p>
+      You are responsible for all content that you post, share, or otherwise make available on the
+      Service ("User Content"). You retain ownership rights in your User Content, but by posting it,
+      you grant us a non-exclusive license to use, copy, and display your User Content as necessary
+      to provide the Service.
+    </p>
+
+    <h2>7. Termination</h2>
+    <p>
+      We reserve the right to suspend or terminate your account at our sole discretion, without
+      notice, for conduct that we believe violates these Terms or is harmful to other users of the
+      Service, us, or third parties, or for any other reason. Upon request, we will delete your
+      personal data.
+    </p>
+
+    <h2>8. Disclaimers</h2>
+    <p>
+      The Service is provided "as-is" and we make no warranties, either express or implied,
+      regarding its availability, accuracy, or functionality.
+    </p>
+
+    <h2>9. Limitation of Liability</h2>
+    <p>
+      Subject to applicable laws, including provisions allowing individuals to seek compensation for
+      damages, in no event shall Cosaga be liable for any indirect, incidental, special, or
+      consequential damages arising out of or in connection with the Service.
+    </p>
+
+    <h2>10. Governing Law and Jurisdiction</h2>
+    <p>
+      These Terms shall be governed by the laws of the State of [Your State], without respect to its
+      conflict of laws principles.
+    </p>
+
+    <h2>11. Amendments</h2>
+    <p>
+      We reserve the right to modify or replace these Terms at any time. If a revision is material,
+      we will try to provide at least 30 days' notice prior to any new terms taking effect and will
+      request renewed consent for any changes involving the processing of personal data.
+    </p>
+
+    <h2>12. Contact Information</h2>
+    <p>For any questions about these Terms, please contact us at:</p>
+    <ul>
+      <li>Email: tommy@cosaga.app</li>
+      <li>Phone: (737) 471-9337</li>
+    </ul>
+  </div>
+</div>
+
+<style>
+  h2 {
+    font-size: 1.5rem;
+    margin-bottom: 0.75rem;
+    margin-top: 1.5rem;
+  }
+</style>
